92b85a2528b6ab3d039cb9066950e654ac8991c0
D-Reyes216 <davepr216@gmail.com>
2023-11-10 22:31:52 -0800

    added styling; added items to menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import './App.css';
 import Header from "./components/Header.jsx";
-import Center from "./components/Center.jsx"
+import Orders from './components/orders.jsx';
+import Menu from './components/Menu.jsx';
 
 
 function App() {
   return (
   <>
     <div><Header /></div>
-    <div><Center /></div>
-  </>);
-}
+    <div className="container">
+      <div><Orders /></div>
+      <div className="menu-container">
+          <div>
+              <div className="menu-header">Menu</div>
+          <div className="menu-list"><Menu /></div>
+        </div>
+      </div>
+    </div>
+
+  </>)}
 
 export default App;
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,7 @@
 function Header() {
     return <div className="header">
             <img src="src\assets\low-poly-burger-with-plate.png" alt="low poly burger with plate" />
-            <h3>Welcome to React Mini-Restaurant</h3>
+            <h3>Welcome to React Restaurant</h3>
         </div>;
 }
 

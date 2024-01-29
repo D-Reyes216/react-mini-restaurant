e03c0a4867a3befefe4100928b9d0c7deafc17d7
D-Reyes216 <davepr216@gmail.com>
2024-01-29 13:46:08 -0800

    changed img file paths x2

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,6 @@
 function Header() {
     return <div className="header">
-            <img src="src\assets\low-poly-burger-with-plate.png" alt="low poly burger with plate" />
+            <img src="/src/assets/low-poly-burger-with-plate.png" alt="low poly burger with plate" />
             <h3>Welcome to React Restaurant</h3>
         </div>;
 }
